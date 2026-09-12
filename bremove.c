fb7fa5f48c52d8921f7a34f303281a540d1a08e5
AravindhkrishnaAL <aravindhkrishnaal@gmail.com>
2026-09-12 21:25:05 +0530

    Create bremove.c

---
+++ bremove.c
@@ -0,0 +1,24 @@
+#include"header.h"
+void bremove(struct st **ptr)
+{
+	char op1;
+	printf("\n");
+	printf("\t\t+----------------------------+\n");
+        printf("\t\t|        REMOVE BOOK         |\n");
+	display(*ptr);
+	if(flag){
+		flag=0;
+	printf("\nDELETE THIS BOOK (Y/N) : ");
+        scanf(" %c",&op1);
+ 	if((op1|32)!='y')
+        {
+        	printf("\nDELETE OPERATION CANCELLED\n");
+        	return;
+        }
+	else
+		del_id(ptr);
+	}
+	
+	      
+
+}
